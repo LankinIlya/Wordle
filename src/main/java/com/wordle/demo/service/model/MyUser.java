5f6d99b36afbb1a357086b579ae16fed33677056
BrovkoRoman <100349968+BrovkoRoman@users.noreply.github.com>
2024-10-07 19:34:59 +0400

    add basic authorization
    * add basic authorization

---
+++ src/main/java/com/wordle/demo/service/model/MyUser.java
@@ -0,0 +1,46 @@
+package com.wordle.demo.service.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class MyUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+    public MyUser() {}
+    public MyUser(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
