4be2d9da3162e0f8eb5d8dc335b80461e1714f11
shnirelman <100349934+shnirelman@users.noreply.github.com>
2024-10-13 20:50:11 +0400

    added liquibase

---
+++ src/main/resources/db/changelog/changeset/create-users-table.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+--changeset shnirelman:create-words-table
+
+CREATE TABLE IF NOT EXISTS users (
+    id bigint,
+    login varchar(32) UNIQUE,
+    password varchar(256),
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to admin;
+
+CREATE SEQUENCE IF NOT EXISTS users_seq
+    INCREMENT 50
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
+ALTER SEQUENCE public.users_seq
+    OWNER TO admin;
