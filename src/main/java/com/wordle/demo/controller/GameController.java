f439950df391baad8b1041051840ac4b5b82253b
Alexey <shnirelmana@gmail.com>
2024-10-21 22:25:34 +0400

    added handling game operations

--- src/main/java/com/wordle/demo/controller/GameController.java
+++ src/main/java/com/wordle/demo/controller/GameController.java
@@ -1,24 +1,32 @@
 package com.wordle.demo.controller;
 
+import com.wordle.demo.controller.dto.GameDto;
 import com.wordle.demo.controller.dto.TryWordDto;
 import com.wordle.demo.controller.dto.TryWordResponseDto;
+import com.wordle.demo.exception.GameAlreadyFinishedException;
+import com.wordle.demo.exception.GameNotFoundException;
 import com.wordle.demo.exception.IncorrectGuessException;
 import com.wordle.demo.exception.WordNotFoundException;
 import com.wordle.demo.service.GameService;
+import com.wordle.demo.service.WordService;
+import com.wordle.demo.service.model.Game;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/game")
 public class GameController {
-    private GameService gameService;
+    private final GameService gameService;
+    private final WordService wordService;
 
-    public GameController(GameService gameService) {
+    public GameController(GameService gameService, WordService wordService) {
         this.gameService = gameService;
+        this.wordService = wordService;
     }
 
     @GetMapping("/new_game")
     public String newGame() {
-        gameService.newGame();
+        Long userId = 1L;
+        gameService.newGame(userId);
         return "ok";
     }
 
@@ -26,7 +34,7 @@ public class GameController {
     public TryWordResponseDto tryWord(@RequestBody TryWordDto tryWordDto) {
         try {
             return new TryWordResponseDto(
-                gameService.tryWord(tryWordDto.word())
+                gameService.tryWord(tryWordDto.gameId(), tryWordDto.word())
             );
         }
         catch (WordNotFoundException ex) {
@@ -34,7 +42,17 @@ public class GameController {
         }
         catch (IncorrectGuessException ex) {
             return new TryWordResponseDto(1);
+        } catch (GameAlreadyFinishedException e) {
+            return new TryWordResponseDto(2);
+        } catch (GameNotFoundException e) {
+            return new TryWordResponseDto(2);
         }
+    }
 
+    @GetMapping("/get-game")
+    public GameDto getGameState() throws GameNotFoundException {
+        Long userId = 0L;
+        Game game = gameService.getGameByUser(userId);
+        return new GameDto(game, gameService, wordService);
     }
 }
